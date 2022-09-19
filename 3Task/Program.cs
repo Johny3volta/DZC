510d1c4101bce8b8cafbf69c45be3f03ea58a27d
3v <jarovcev@mail.ru>
2022-09-19 16:46:51 +0300

    3.2

--- 3Task/Program.cs
+++ 3Task/Program.cs
@@ -1,11 +1,11 @@
-﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+﻿// Задайте массив вещественных(дробных) чисел. Найдите разницу между максимальным и минимальным элементов массива.
 Console.Clear();
 Console.WriteLine("Найдите разницу между максимальным и минимальным элементов массива из вещественных чисел" );
 
 double[] arrayRealNumbers = new double[10];
   for (int i = 0; i < arrayRealNumbers.Length; i++ )
   {
-    arrayRealNumbers[i] = new Random().Next(1, 10);
+    arrayRealNumbers[i] = new Random().NextDouble() *10;
     Console.Write(arrayRealNumbers[i] + " ");
   }
 
